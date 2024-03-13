3f4fb1283108c96ba902d2500c04b21d827b4b7b
sponja23 <tomas.spognardi@gmail.com>
2024-03-12 21:08:35 -0300

    feat(dependencies): Made file path in ModuleDependencyInfo relative to project root

--- pytest_git_diff/dependency_graph/dependencies.py
+++ pytest_git_diff/dependency_graph/dependencies.py
@@ -61,12 +61,16 @@ def pydeps_dependency_dict(path: Path) -> Dict[str, Any]:
 @dataclass
 class ModuleDependencyInfo:
     name: str
-    path: str
+    path: Path
     dependencies: List[str]
     dependents: List[str]
 
     @staticmethod
-    def from_pydeps_dict_entry(entry: Dict[str, Any]) -> "ModuleDependencyInfo":
+    def from_pydeps_dict_entry(
+        entry: Dict[str, Any],
+        *,
+        project_root: Path,
+    ) -> "ModuleDependencyInfo":
         """
         Parse a single entry from the pydeps dependency dictionary
 
@@ -76,13 +80,34 @@ class ModuleDependencyInfo:
         Returns:
             A dictionary with the following keys:
             - "name": The name of the module
-            - "path": The path to the module
+            - "path": The path to the module, relative to the root of the project
             - "imports": A list of the modules that the module imports
             - "imported_by": A list of the modules that import the module
         """
         return ModuleDependencyInfo(
             name=entry["name"],
-            path=entry["path"],
+            # Convert the path to be relative to the root of the project
+            path=Path(entry["path"]).relative_to(project_root),
             dependencies=entry.get("imports", []),
             dependents=entry.get("imported_by", []),
         )
+
+
+def get_dependencies(project_root: Path) -> Dict[str, ModuleDependencyInfo]:
+    """
+    Get the dependencies of the project
+
+    Args:
+        project_root: The path to the root of the project
+
+    Returns:
+        A dictionary mapping module names to their dependency information
+    """
+    dependency_dict = pydeps_dependency_dict(project_root)
+    return {
+        module_name: ModuleDependencyInfo.from_pydeps_dict_entry(
+            entry,
+            project_root=project_root,
+        )
+        for module_name, entry in dependency_dict.items()
+    }
