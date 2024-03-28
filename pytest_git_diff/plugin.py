8abf226f4c3b0d586f3f43e8149d1bca1224465a
sponja23 <tomas.spognardi@gmail.com>
2024-03-28 17:25:27 -0300

    feat(plugin): Added '--only-affected-last-commit' option

--- pytest_git_diff/plugin.py
+++ pytest_git_diff/plugin.py
@@ -13,7 +13,14 @@ def pytest_addoption(parser: pytest.Parser) -> None:
         action="store",
         default=None,
         type=str,
-        help="Filter the collected tests, keeping only those affected by the given revision",
+        help="Filter the collected tests, keeping only those affected by the given commit range."
+        " The range should be in the format 'FROM_REV..TO_REV'. If 'TO_REV' is omitted, it defaults to 'HEAD'.",
+    )
+
+    group.addoption(
+        "--only-affected-last-commit",
+        action="store_true",
+        help="Filter the collected tests, keeping only those affected by the last commit",
     )
 
 
@@ -23,14 +30,21 @@ def pytest_collection_modifyitems(
     items: List[pytest.Item],
 ) -> None:
     only_affected_by = config.getoption("--only-affected-by")
-    if only_affected_by is None:
+    only_affected_last_commit = config.getoption("--only-affected-last-commit")
+
+    if not only_affected_by and not only_affected_last_commit:
         return
+
+    if only_affected_last_commit:
+        only_affected_by = "HEAD~1..HEAD"
 
     if ".." not in only_affected_by:
         from_rev = only_affected_by
         to_rev = "HEAD"
     else:
         from_rev, to_rev = only_affected_by.split("..")
+        if not to_rev:
+            to_rev = "HEAD"
 
     affected_files = get_affected_files(session.startpath, from_rev, to_rev)
 
